32e08cb447743d3e3a166ffdf6f0cb2ffe8015c9
adityacodes <adityapadhi1996@gmail.com>
2017-01-31 00:55:30 +0530

    Added Model, Migration and Controller for Test

--- database/migrations/2017_01_31_004444_create_tests_table.php
+++ database/migrations/2017_01_31_004444_create_tests_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreteTnpTable extends Migration
+class CreateTestsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,32 +12,26 @@ class CreteTnpTable extends Migration
      */
     public function up()
     {
-        Schema::create('t_n_p_s', function(Blueprint $table)
-        {
+        Schema::create('tests', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('regdno')->unique();
             $table->string('branch');
             $table->string('dob',10);
             $table->string('gender',10);
-
             $table->string('tenthyear',5);
             $table->string('tenthpercent',5);
             $table->string('tenthboard',6);
-
             $table->string('twelthyear',5);
             $table->string('twelthpercent',5);
             $table->string('twelthboard',6);
-
             $table->string('diplomayear',5);
             $table->string('diplomapercent',5);
             $table->string('diplomaboard',6);
-
             $table->string('cgpa',5);
             $table->string('backlog',10);
             $table->string('paddress',250);
             $table->string('praddress',250);
-
             $table->string('fname',50);
             $table->string('foccupation',50);
             $table->string('mname',50);
@@ -46,6 +40,7 @@ class CreteTnpTable extends Migration
             $table->string('roccupation',50);
             $table->string('internships',250);
             $table->string('email')->unique();
+            $table->string('cv');
             $table->timestamps();
         });
     }
@@ -57,6 +52,6 @@ class CreteTnpTable extends Migration
      */
     public function down()
     {
-        Schema::drop('t_n_p_s');
+        Schema::drop('tests');
     }
 }
